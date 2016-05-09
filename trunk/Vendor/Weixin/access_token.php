d84dee36ede5c008058e3357296a477351aa4a28
mymusic56 <961272668@qq.com>
2016-05-09 20:55:50 +0800

    qq

---
+++ trunk/Vendor/Weixin/access_token.php
@@ -0,0 +1,2 @@
+<?php exit();?>
+{"access_token":"QZejCQ2meMkgP9ow6CjEO1z-HA9YTr6lJyJ9okfX5tXlYGMcr2Pw5vFfnW8LD4DqusHY8GuE0YgrPdmgKDenK5eiPyvuhl3xS-zjBBwvlXk20HKclmoyPJQryB8JPartLUDaADAXGP","expire_time":1462378557}
