214c01d1547982e987c58e62effa030da86f8adc
engine56 <961272668@qq.com>
2017-09-24 23:32:50 +0800

    图片转换成base64

---
+++ function/base64image.php
@@ -0,0 +1,13 @@
+<?php
+$img = 'C:\Users\ZangShengJi\Desktop\444.png';
+$base64_img = base64EncodeImage($img);
+
+echo '<img src="' . $base64_img . '" />';
+
+function base64EncodeImage ($image_file) {
+    $base64_image = '';
+    $image_info = getimagesize($image_file);
+    $image_data = fread(fopen($image_file, 'r'), filesize($image_file));
+    $base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+    return $base64_image;
+}
