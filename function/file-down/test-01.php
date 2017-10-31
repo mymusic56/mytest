e0ff26e0768e743d2ae8aa568adc487276fc98aa
engine56 <961272668@qq.com>
2017-10-31 23:52:07 +0800

    PHP文件下载

---
+++ function/file-down/test-01.php
@@ -0,0 +1,15 @@
+<?php
+function downfile($fileName,$filePath){
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename='.$fileName);
+	header('Content-Transfer-Encoding: binary');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($filePath));
+	readfile($filePath);//读取文件并写入到输出缓冲
+}
+$fileName = '640.jpg';
+$filePath = 'D:\Workspace-PHP\mytest\Public\img\640.jpg';
+downfile($fileName,$filePath);
