f17013d8329c87ebe8461c52534032f3495745f0
mymusic56 <961272668@qq.com>
2019-02-15 21:43:32 +0800

    --

---
+++ function/magic_func/String.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangshengji
+ * Date: 2019/2/14
+ * Time: 22:31
+ */
+
+class String
+{
+
+}
---
+++ function/magic_func/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangshengji
+ * Date: 2019/2/14
+ * Time: 22:08
+ */
