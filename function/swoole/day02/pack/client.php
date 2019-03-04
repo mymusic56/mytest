e68b7f506e5e3526f7ab0ae973bd713484dc2cf1
engine56 <zhangshengji56@163.com>
2019-03-04 21:08:47 +0800

    swoole粘包

--- function/swoole/day02/pack/client.php
+++ function/swoole/day02/pack/client.php
@@ -5,3 +5,22 @@
  * Date: 2019/2/26
  * Time: 21:34
  */
+
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+$client->on('receive', function (swoole_client $cli, string $data) {
+    echo $data;
+});
+
+$client->on("error", function (swoole_client $cli) {
+    echo "error\n";
+});
+$client->on("close", function (swoole_client $cli) {
+    echo "Connection close\n";
+});
+
+$data = str_repeat('a', 1024*1024*3);
+$data = pack('N', $data);
+//$client->sendto('127.0.0.1', 9502, $data);//Swoole\Client::sendto(): only supports SWOOLE_SOCK_UDP or SWOOLE_SOCK_UDP6, 4.0.4
+
+$client->connect('127.0.0.1', 9502);//Fatal error: Swoole\Client::connect(): no 'onConnect' callback function.
+$client->send($data);
--- function/swoole/day02/pack/server.php
+++ function/swoole/day02/pack/server.php
@@ -5,3 +5,26 @@
  * Date: 2019/2/26
  * Time: 21:34
  */
+
+$server = new Swoole\Server('0.0.0.0', 9502);
+$server->set([
+    'open_length_check' => true,
+    'package_length_type' => 'N',
+    'package_max_length' => 1024*1024*2,
+    'package_body_offset' => 4,
+]);
+
+$server->on('connect', function (swoole_server $server, int $fd, int $reactorId) {
+    echo "client {$fd} connected.";
+});
+
+$server->on('receive', function (swoole_server $server, int $fd, int $reactorId, string $data) {
+    $d = unpack('N', $data);
+    echo $d;
+});
+
+$server->on('close', function (swoole_server $server, int $fd, int $reactorId) {
+
+});
+
+$server->start();
